9bf9fb3549d5a94782448bcb0474a28c484572a9
Wes Moser <Moser.Wesley@gmail.com>
2014-01-27 20:58:39 -0500

    Initial creation.

--- WhereAmI/WhereAmIViewController.h
+++ WhereAmI/WhereAmIViewController.h
@@ -7,7 +7,11 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <CoreLocation/CoreLocation.h>
 
-@interface WhereAmIViewController : UIViewController
+@interface WhereAmIViewController : UIViewController <CLLocationManagerDelegate>
+{
+    CLLocationManager *locationManager;
+}
 
 @end
--- WhereAmI/WhereAmIViewController.m
+++ WhereAmI/WhereAmIViewController.m
@@ -17,13 +17,27 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	locationManager = [[CLLocationManager alloc] init];
+    [locationManager setDelegate:self];
+    [locationManager setDesiredAccuracy:kCLLocationAccuracyBest];
+    [locationManager startUpdatingLocation];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations
+{
+    CLLocation *location = [locations objectAtIndex:0];
+    NSLog(@"%@", [location description]);
+}
+
+- (void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error
+{
+    NSLog(@"Could not find the location %@", [error localizedDescription]);
 }
 
 @end
